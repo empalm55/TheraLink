07ef0f283edf0657f33682c8d962dd869f326fd0
empalm55 <epalmer1@harding.edu>
2024-11-20 21:32:25 -0600

    Update PotToAngle.ino
    updated to remove bluetooth and add in an led indicator to show "Recording"

--- PotToAngle/PotToAngle.ino
+++ PotToAngle/PotToAngle.ino
@@ -1,24 +1,32 @@
-/*
-  AnalogReadSerial
-
-  Reads an analog input on pin 0, prints the result to the Serial Monitor.
-
-  Some of this is based on a tutorial from Arduino:
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/AnalogReadSerial
-*/
+float sensorValue;
+float angle;
+#define potPin A0
+#define ledPin 2
 
 // the setup routine runs once when you press reset:
 void setup() {
-  // initialize serial communication at 9600 bits per second:
+  // initialize serial communication at 9600 bits per second
+  pinMode(potPin,INPUT); //potentiometer
+  pinMode(ledPin,OUTPUT); //LED recorder indicator
   Serial.begin(9600);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  // read the input on analog pin 0:
-  float sensorValue = analogRead(A0);
-  float angle=(sensorValue/1023)*265; //total angle range of potentiometer I am using is about 265 degrees
-  // print out the value read:
-  Serial.println(angle);
+ if (Serial.read()=='a'){ 
+  while(Serial.read() !='s'){  //until you tell it to stop
+    digitalWrite(ledPin, HIGH);
+    // read the input on analog pin 0:
+    sensorValue = analogRead(potPin);
+    angle=(sensorValue/1023)*265;
+    // print out the value you read:
+    Serial.println(angle);
+    delay(500);
+    digitalWrite(ledPin,LOW);
+    delay(500);
+  }
+  digitalWrite(ledPin, LOW);
+ }
+
   delay(1000);  // delay in between reads for stability
 }
